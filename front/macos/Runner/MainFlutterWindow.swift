08bbfe7adcf821d721483d5626d750311ebc0030
rlatmdgus388 <rlatmdgus388@naver.com>
2025-05-13 23:15:26 +0900

    first commit

---
+++ front/macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,16 @@
+import Cocoa
+import FlutterMacOS
+import bitsdojo_window_macos
+
+class MainFlutterWindow: BitsdojoWindow {
+  override func awakeFromNib() {
+    let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    super.awakeFromNib()
+  }
+}
