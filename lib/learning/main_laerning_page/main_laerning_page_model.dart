906b4251f6b492cb3b5ef295fd6f6d3e6331e1b6
jaeminsd <sodlfma159@gmail.com>
2025-05-28 20:34:33 +0900

    Flutter 05/28

---
+++ lib/learning/main_laerning_page/main_laerning_page_model.dart
@@ -0,0 +1,16 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'main_laerning_page_widget.dart' show MainLaerningPageWidget;
+import 'package:ff_theme/flutter_flow/flutter_flow_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class MainLaerningPageModel extends FlutterFlowModel<MainLaerningPageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ lib/settings/main_settings_page/main_settings_page_model.dart
@@ -0,0 +1,17 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'main_settings_page_widget.dart' show MainSettingsPageWidget;
+import 'package:ff_theme/flutter_flow/flutter_flow_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class MainSettingsPageModel extends FlutterFlowModel<MainSettingsPageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
