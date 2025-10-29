3515cc85e0b079ce0690243bc3a5429f0d9417ce
rlatmdgus388 <rlatmdgus388@naver.com>
2025-10-30 03:21:39 +0900

    Drawer 컴포넌트 버그 수정

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -65,6 +65,7 @@ function DrawerContent({
         )}
         {...props}
       >
+        <DrawerTitle className="sr-only">Drawer</DrawerTitle>
         <div className="bg-muted mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-full group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
         {children}
       </DrawerPrimitive.Content>
