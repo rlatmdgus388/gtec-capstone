6b2a7c91a966a710aee1fc119f8e963cd9a3c057
rlatm <rlatmdgus388@naver.com>
2025-10-30 10:36:53 +0900

    사진으로 추가 UI 버그 수정

--- components/camera/image-selection-modal.tsx
+++ components/camera/image-selection-modal.tsx
@@ -13,21 +13,13 @@ interface ImageSelectionModalProps {
 
 export function ImageSelectionModal({ open, onClose, onCameraSelect, onGallerySelect }: ImageSelectionModalProps) {
   return (
-    <Dialog open={open} onOpenChange={() => {}}>
+    <Dialog open={open} onOpenChange={onClose}>
       <DialogContent
         className="w-[calc(100vw-3rem)] max-w-sm mx-auto rounded-2xl p-6 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
         onPointerDownOutside={(e) => e.preventDefault()}
       >
         <DialogHeader className="relative pb-4">
           <DialogTitle className="text-center text-lg font-semibold text-gray-900 pr-8">이미지 선택</DialogTitle>
-          <Button
-            variant="ghost"
-            size="sm"
-            onClick={onClose}
-            className="absolute right-0 top-0 h-6 w-6 p-0 hover:bg-gray-100 rounded-full"
-          >
-            <X size={16} />
-          </Button>
         </DialogHeader>
 
         <div className="space-y-4">
