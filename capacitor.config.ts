edfe4322ebdc3642e91cc38fc147fca878e65ef4
rlatmdgus388 <rlatmdgus388@naver.com>
2025-11-19 14:58:25 +0900

    apk 수정 테스트1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,15 @@ const config: CapacitorConfig = {
     url: 'https://gtec-capstone-6tbh.vercel.app', // ✅ 배포된 Vercel 주소
     cleartext: false, // HTTPS니까 false
   },
+  plugins: {
+    StatusBar: {
+      // ✅ 상태바가 WebView 위에 겹치지 않게
+      overlaysWebView: false,
+      // 선택: 상태바 글자 색 / 배경색 (원하는 대로)
+      style: 'DARK',
+      backgroundColor: '#000000',
+    }
+  }
 };
 
 export default config;
